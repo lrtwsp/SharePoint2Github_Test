b5e63b95e8e29d8bdde24046eb9ee350f0f564d7
lrtwsp <lucas.thompson@wsp.com>
2023-11-29 16:22:39 -0800

    make_repo_magic.py now extracts the vba to text files. Extensions unfortunately are harder.

--- make_repo_magic.py
+++ make_repo_magic.py
@@ -2,18 +2,13 @@ import pandas as pd
 import os
 import git
 import argparse
-from openpyxl import load_workbook
-import zipfile
-import tempfile
-import shutil
 import os
-from git import Repo, exc
-from dotenv import load_dotenv
+from git import exc
 from oletools.olevba import VBA_Parser
 
 def extract_vba_to_text(excel_file, output_dir):
     """
-    Extracts VBA code from an Excel file and saves it as text files.
+    Extracts VBA code from an Excel file and saves it as text files with appropriate extensions.
     Args:
     excel_file (str): The path to the Excel file.
     output_dir (str): The directory where the VBA code will be saved as text.
@@ -22,35 +17,12 @@ def extract_vba_to_text(excel_file, output_dir):
 
     if vba_parser.detect_vba_macros():
         for (filename, stream_path, vba_filename, vba_code) in vba_parser.extract_macros():
-            with open(os.path.join(output_dir, vba_filename), 'w') as file:
+            file_extension = '.vba'  # Default extension
+            # Additional logic can be added here to determine the correct extension
+            with open(os.path.join(output_dir, vba_filename + file_extension), 'w') as file:
                 file.write(vba_code)
 
     vba_parser.close()
-
-
-def extract_vba(excel_file, vba_output_dir):
-    """
-    Extracts VBA code from an Excel file and saves it to a specified directory.
-    Args:
-    excel_file (str): The path to the Excel file.
-    vba_output_dir (str): The directory where the VBA code will be saved.
-    """
-    wb = load_workbook(excel_file, keep_vba=True)
-    if not wb.vba_archive:
-        return
-
-    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-        wb.save(tmp_file.name)
-
-    vba_folder_name = os.path.splitext(os.path.basename(excel_file))[0] + '_VBA'
-    vba_output_path = os.path.join(vba_output_dir, vba_folder_name)
-
-    with zipfile.ZipFile(tmp_file.name, 'r') as zip_ref:
-        for file in zip_ref.namelist():
-            if file.startswith('xl/vbaProject.bin'):
-                zip_ref.extract(file, vba_output_path)
-
-    os.remove(tmp_file.name)
 
 def convert_excel_to_csv(excel_file, csv_file):
     """
@@ -85,7 +57,7 @@ def git_operations(repo_dir, commit_message):
 
 def main(excel_files, repo_dir):
     for file in excel_files:
-        csv_output_dir = os.path.join(repo_dir, 'forgithub')
+        csv_output_dir = os.path.join(repo_dir, 'extracted_data')
         vba_output_dir = csv_output_dir  # Saving VBA code in the same directory
 
         if not os.path.exists(csv_output_dir):
